e8fecea514446406595973570ae083ab7db50a37
Vladimir Shevtsov <shevv920@gmail.com>
2019-11-25 02:14:08 +0300

    formatting

--- src/main/scala/votbot/event/handlers/BaseEventHandler.scala
+++ src/main/scala/votbot/event/handlers/BaseEventHandler.scala
@@ -26,65 +26,65 @@ trait BaseEventHandler extends EventHandler {
       api   <- ZIO.environment[Api]
       state <- ZIO.access[BotState](_.state)
       _ <- ZIO.whenCase(event) {
-        case Ping(Some(args)) =>
-          api.enqueueOutMessage(RawMessage(Command.Pong, args))
-        case Ping(None) =>
-          api.enqueueOutMessage(RawMessage(Command.Pong))
-        case Numeric(RPL_YOURHOST, _, prefix) =>
-          ZIO.unit
-        case Numeric(ERR_NICKNAMEINUSE, _, prefix) =>
-          for {
-            n       <- ZIO.access[Random](_.random.nextInt(99))
-            newNick = cfg.bot.nick + n
-            _       <- api.enqueueOutMessage(RawMessage(Command.Nick, newNick))
-            _       <- state.update(s => s.copy(nick = newNick))
-          } yield ()
-        case Connected =>
-          val nickCmd = RawMessage(Command.Nick, cfg.bot.nick)
-          val userCmd = RawMessage(Command.User, cfg.bot.userName, "*", "*", cfg.bot.realName)
-          api.enqueueOutMessage(nickCmd, userCmd)
-        case Welcome(nick, host) =>
-          api
-            .enqueueOutMessage(RawMessage(Command.Join, cfg.bot.autoJoinChannels.mkString(",")))
-            .flatMap(_ => state.update(_.copy(nick = nick)))
-        case BotJoin(chName) =>
-          api.addChannel(Channel(chName, List.empty, Set.empty))
-        case BotPart(channel) =>
-          api.removeChannel(channel)
-        case Join(name, channel) =>
-          for {
-            user <- api.getOrCreateUser(name)
-            _    <- api.addChannelMember(channel, ChannelMember(user, List.empty))
-          } yield ()
-        case Part(user, channel, reason) =>
-          api.removeChannelMember(channel, user)
-        case NamesList(chName, members) =>
-          for {
-            channelMembers <- ZIO.foreach(members) { tuple =>
+            case Ping(Some(args)) =>
+              api.enqueueOutMessage(RawMessage(Command.Pong, args))
+            case Ping(None) =>
+              api.enqueueOutMessage(RawMessage(Command.Pong))
+            case Numeric(RPL_YOURHOST, _, prefix) =>
+              ZIO.unit
+            case Numeric(ERR_NICKNAMEINUSE, _, prefix) =>
               for {
-                user   <- api.getOrCreateUser(tuple._1)
-                modes  = tuple._2
-                member = ChannelMember(user, modes)
-              } yield member
-            }
-            _ <- ZIO.foreach(channelMembers)(api.addChannelMember(chName, _))
-          } yield ()
-        case Numeric(RPL_ENDOFNAMES, args, prefix) =>
-          ZIO.unit
-        case Quit(user, reason) =>
-          for {
-            user <- api.findUser(user)
-            _ <- ZIO.when(user.nonEmpty) {
-              for {
-                _        <- api.removeUser(user.get)
-                channels = user.get.channels
-                _ <- ZIO.foreach(channels) { ch =>
-                  api.removeChannelMember(ch.name, user.get.name)
-                }
+                n       <- ZIO.access[Random](_.random.nextInt(99))
+                newNick = cfg.bot.nick + n
+                _       <- api.enqueueOutMessage(RawMessage(Command.Nick, newNick))
+                _       <- state.update(s => s.copy(nick = newNick))
               } yield ()
-            }
-          } yield ()
-      }
+            case Connected =>
+              val nickCmd = RawMessage(Command.Nick, cfg.bot.nick)
+              val userCmd = RawMessage(Command.User, cfg.bot.userName, "*", "*", cfg.bot.realName)
+              api.enqueueOutMessage(nickCmd, userCmd)
+            case Welcome(nick, host) =>
+              api
+                .enqueueOutMessage(RawMessage(Command.Join, cfg.bot.autoJoinChannels.mkString(",")))
+                .flatMap(_ => state.update(_.copy(nick = nick)))
+            case BotJoin(chName) =>
+              api.addChannel(Channel(chName, List.empty, Set.empty))
+            case BotPart(channel) =>
+              api.removeChannel(channel)
+            case Join(name, channel) =>
+              for {
+                user <- api.getOrCreateUser(name)
+                _    <- api.addChannelMember(channel, ChannelMember(user, List.empty))
+              } yield ()
+            case Part(user, channel, reason) =>
+              api.removeChannelMember(channel, user)
+            case NamesList(chName, members) =>
+              for {
+                channelMembers <- ZIO.foreach(members) { tuple =>
+                                   for {
+                                     user   <- api.getOrCreateUser(tuple._1)
+                                     modes  = tuple._2
+                                     member = ChannelMember(user, modes)
+                                   } yield member
+                                 }
+                _ <- ZIO.foreach(channelMembers)(api.addChannelMember(chName, _))
+              } yield ()
+            case Numeric(RPL_ENDOFNAMES, args, prefix) =>
+              ZIO.unit
+            case Quit(user, reason) =>
+              for {
+                user <- api.findUser(user)
+                _ <- ZIO.when(user.nonEmpty) {
+                      for {
+                        _        <- api.removeUser(user.get)
+                        channels = user.get.channels
+                        _ <- ZIO.foreach(channels) { ch =>
+                              api.removeChannelMember(ch.name, user.get.name)
+                            }
+                      } yield ()
+                    }
+              } yield ()
+          }
       handlers <- customHandlers.get
       _        <- ZIO.foreach(handlers)(handler => handler.handle(event))
     } yield ()
