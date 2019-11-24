6e158954e5e5a47a64ffb49065305d811c6f2d0a
Vladimir Shevtsov <shevv920@gmail.com>
2019-11-25 01:44:17 +0300

    BaseEventHandler specs WIP

--- src/test/scala/votbot/event/BaseEventHandlerSpec.scala
+++ src/test/scala/votbot/event/BaseEventHandlerSpec.scala
@@ -1,15 +1,16 @@
 package votbot.event
 import votbot.Api
-import votbot.event.Event.Ping
+import votbot.event.Event.{ Connected, Ping }
 import votbot.event.handlers.BaseEventHandler
-import zio.ZIO
-import zio.test._
-import Assertion._
+import votbot.model.Irc
 import votbot.model.Irc.{ Command, RawMessage }
+import zio.ZIO
+import zio.test.Assertion._
+import zio.test._
 
 object BaseEventHandlerSpec {
 
-  val tests = suite("BaseEventHandler ") {
+  val tests = suite("BaseEventHandler")(
     testM("response for ping request with/with no args ") {
       for {
         api         <- ZIO.environment[Api]
@@ -23,6 +24,15 @@ object BaseEventHandlerSpec {
         pongArgs    <- api.dequeueOutMessage()
       } yield assert(pongNoArgs, equalTo(RawMessage(Command.Pong))) &&
         assert(pongArgs, equalTo(RawMessage(Command.Pong, Vector(args))))
+    },
+    testM("send nick and user commands on Connected Event") {
+      for {
+        api     <- ZIO.environment[Api]
+        handler <- ZIO.environment[BaseEventHandler]
+        _       <- handler.handle(Connected)
+        nickCmd <- api.dequeueOutMessage()
+        userCmd <- api.dequeueOutMessage()
+      } yield assert(nickCmd.cmd, equalTo(Irc.Command.Nick)) && assert(userCmd.cmd, equalTo(Irc.Command.User))
     }
-  }
+  )
 }
