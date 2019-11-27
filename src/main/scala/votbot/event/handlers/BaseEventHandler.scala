cad4ce1d86d233380fc37029c66f195a27f73b18
Vladimir Shevtsov <shevv920@gmail.com>
2019-11-27 18:21:32 +0300

    formatting

--- src/main/scala/votbot/event/handlers/BaseEventHandler.scala
+++ src/main/scala/votbot/event/handlers/BaseEventHandler.scala
@@ -1,8 +1,8 @@
 package votbot.event.handlers
 import votbot.event.Event._
 import votbot.event.EventHandler
+import votbot.model.Irc
 import votbot.model.Irc.Numeric.{ERR_NICKNAMEINUSE, RPL_ENDOFNAMES, RPL_YOURHOST}
-import votbot.model.Irc._
 import votbot.{Api, BotState, Configuration}
 import zio.random.Random
 import zio.{Ref, ZIO}
@@ -27,39 +27,39 @@ trait BaseEventHandler extends EventHandler {
       state <- ZIO.access[BotState](_.state)
       _ <- ZIO.whenCase(event) {
             case Ping(Some(args)) =>
-              api.enqueueOutMessage(RawMessage(Command.Pong, args))
+              api.enqueueOutMessage(Irc.RawMessage(Irc.Command.Pong, args))
             case Ping(None) =>
-              api.enqueueOutMessage(RawMessage(Command.Pong))
+              api.enqueueOutMessage(Irc.RawMessage(Irc.Command.Pong))
             case Numeric(RPL_YOURHOST, _, prefix) =>
               ZIO.unit
             case Numeric(ERR_NICKNAMEINUSE, _, prefix) =>
               for {
                 n       <- ZIO.access[Random](_.random.nextInt(99))
                 newNick = cfg.bot.nick + n
-                _       <- api.enqueueOutMessage(RawMessage(Command.Nick, newNick))
+                _       <- api.enqueueOutMessage(Irc.RawMessage(Irc.Command.Nick, newNick))
                 _       <- state.update(s => s.copy(nick = newNick))
               } yield ()
             case Connected =>
-              val nickCmd = RawMessage(Command.Nick, cfg.bot.nick)
-              val userCmd = RawMessage(Command.User, cfg.bot.userName, "*", "*", cfg.bot.realName)
+              val nickCmd = Irc.RawMessage(Irc.Command.Nick, cfg.bot.nick)
+              val userCmd = Irc.RawMessage(Irc.Command.User, cfg.bot.userName, "*", "*", cfg.bot.realName)
               api.enqueueOutMessage(nickCmd, userCmd)
             case Welcome(nick, host) =>
               api
-                .enqueueOutMessage(RawMessage(Command.Join, cfg.bot.autoJoinChannels.mkString(",")))
+                .enqueueOutMessage(Irc.RawMessage(Irc.Command.Join, cfg.bot.autoJoinChannels.mkString(",")))
                 .flatMap(_ => state.update(_.copy(nick = nick)))
             case BotJoin(chName) =>
-              api.addChannel(Channel(chName, List.empty, Set.empty))
+              api.addChannel(Irc.Channel(chName, List.empty, Set.empty))
             case BotPart(channel) =>
-              api.removeChannel(ChannelKey(channel))
+              api.removeChannel(Irc.ChannelKey(channel))
             case Join(name, channel) =>
               for {
                 user  <- api.getOrCreateUser(name)
-                chKey = ChannelKey(channel)
+                chKey = Irc.ChannelKey(channel)
                 _     <- api.addChannelMember(chKey, user)
-                _     <- api.addChannelToUser(chKey, UserKey(user.name))
+                _     <- api.addChannelToUser(chKey, Irc.UserKey(user.name))
               } yield ()
             case Part(user, channel, reason) =>
-              api.removeChannelMember(ChannelKey(channel), UserKey(user))
+              api.removeChannelMember(Irc.ChannelKey(channel), Irc.UserKey(user))
             case NamesList(chName, members) =>
               for {
                 channelMembers <- ZIO.foreach(members) { tuple =>
@@ -68,13 +68,13 @@ trait BaseEventHandler extends EventHandler {
                                      modes = tuple._2
                                    } yield user
                                  }
-                _ <- ZIO.foreach(channelMembers)(api.addChannelMember(ChannelKey(chName), _))
+                _ <- ZIO.foreach(channelMembers)(api.addChannelMember(Irc.ChannelKey(chName), _))
               } yield ()
             case Numeric(RPL_ENDOFNAMES, args, prefix) =>
               ZIO.unit
             case Quit(userName, reason) =>
               for {
-                user <- api.getUser(UserKey(userName))
+                user <- api.getUser(Irc.UserKey(userName))
                 _    <- api.removeUser(user)
               } yield ()
           }
