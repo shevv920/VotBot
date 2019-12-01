72ac5695683830f569a49a98ba7baa292baabbc7
Vladimir Shevtsov <shevv920@gmail.com>
2019-12-01 23:42:40 +0300

    add Cap Ack server message parse test

--- src/test/scala/votbot/MsgParserSpec.scala
+++ src/test/scala/votbot/MsgParserSpec.scala
@@ -15,6 +15,10 @@ object MsgParserSpec {
       MsgParser
         .parse(Irc.Numeric.RPL_WELCOME + " welcome")
         .map(m => assert(m, equalTo(Irc.RawMessage(Irc.Command.Numeric(Irc.Numeric.RPL_WELCOME), Vector("welcome")))))
+    },
+    testM("should parse CAP * ACK msg") {
+      MsgParser.parse("CAP * ACK :cap1 cap2 cap3")
+        .map(m => assert(m, equalTo(Irc.RawMessage(Irc.Command.Cap, Vector("*", "ACK", "cap1 cap2 cap3")))))
     }
   )
 }
